2accc891c760fb0e50a70d334f5f4c96ad1969ee
ManilaPrime <manilastaycation24@gmail.com>
2026-05-27 13:19:52 +0800

    Create config.ts

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAYpg2bjtkjGSCmfO0u030CYl220TAAg-I",
+  authDomain: "unified-booker.firebaseapp.com",
+  projectId: "unified-booker",
+  storageBucket: "unified-booker.firebasestorage.app",
+  messagingSenderId: "165319197503",
+  appId: "1:165319197503:web:8ddbd48540c8d5ba88098a"
+};
