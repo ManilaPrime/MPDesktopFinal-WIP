256d18fbfedab4a37bbcb839724eb83537405723
Jrcz18 <93472623+Jrcz18@users.noreply.github.com>
2026-06-28 12:23:18 +0800

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAYpg2bjtkjGSCmfO0u030CYl220TAAg-I",
-  authDomain: "unified-booker.firebaseapp.com",
-  projectId: "unified-booker",
-  storageBucket: "unified-booker.firebasestorage.app",
-  messagingSenderId: "165319197503",
-  appId: "1:165319197503:web:8ddbd48540c8d5ba88098a"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
